fdb5ef312a00d4d4658cb7e72c267042949964ab
unknown <ansarihuzaifa2806@gmail.com>
2024-12-23 18:56:48 +0530

    daphne kept at top

--- core/settings.py
+++ core/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -43,7 +44,6 @@ INSTALLED_APPS = [
     "channels",
     "rest_framework",
     "chatbot",
-    "daphne",
 ]
 
 MIDDLEWARE = [
